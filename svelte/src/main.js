0a492faf206561604b48846aadca2afe33b108e2
Kornél Horváth <kornel.horvath@me.com>
2021-10-23 17:26:46 +0200

    Init router in svelte template

--- svelte/src/main.js
+++ svelte/src/main.js
@@ -1,5 +1,9 @@
 import './main.css';
 
+import * as Router from '@koffeine/svelte-router';
 import App from './App.svelte';
+
+Router.init([
+]);
 
 new App({ target: document.body }); // eslint-disable-line no-new
