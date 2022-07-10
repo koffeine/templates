ed00846c0b5badf933e7b0f96938c46943f05544
Kornél Horváth <kornel.horvath@me.com>
2022-07-10 14:46:38 +0200

    Refactored test in spring template

--- spring/src/test/java/koffeine/controller/IndexControllerTest.java
+++ spring/src/test/java/koffeine/controller/IndexControllerTest.java
@@ -1,6 +1,5 @@
-package koffeine;
+package koffeine.controller;
 
-import koffeine.controller.IndexController;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
@@ -10,13 +9,13 @@ import static org.springframework.test.web.servlet.request.MockMvcRequestBuilder
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
 
 @WebMvcTest(IndexController.class)
-class IndexControllerTest {
+public class IndexControllerTest {
 
 	@Autowired
-	MockMvc mockMvc;
+	private MockMvc mockMvc;
 
 	@Test
-	void testIndex() throws Exception {
+	public void testIndex() throws Exception {
 		mockMvc
 			.perform(get("/"))
 			.andExpect(status().isOk())
@@ -24,7 +23,7 @@ class IndexControllerTest {
 	}
 
 	@Test
-	void testNotFound() throws Exception {
+	public void testNotFound() throws Exception {
 		mockMvc
 			.perform(get("/not-found"))
 			.andExpect(status().isNotFound())
