02bfc84de197a0218fa5cbbf43e07ccbfd5d814b
Kornél Horváth <kornel.horvath@me.com>
2022-12-10 11:15:45 +0100

    Updated Spring Boot to 3.0.0

--- spring/src/main/java/koffeine/configuration/properties/CorsProperties.java
+++ spring/src/main/java/koffeine/configuration/properties/CorsProperties.java
@@ -1,10 +1,9 @@
 package koffeine.configuration.properties;
 
+import jakarta.validation.constraints.NotEmpty;
 import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.validation.annotation.Validated;
-
-import javax.validation.constraints.NotEmpty;
 
 @Configuration
 @ConfigurationProperties("cors")
