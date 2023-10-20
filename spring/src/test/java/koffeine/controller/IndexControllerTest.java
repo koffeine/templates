de6049905dfa598d13e67596767ded707b109f5c
Kornél Horváth <kornel.horvath@me.com>
2023-10-20 21:44:16 +0200

    Added CORS test

--- spring/src/test/java/koffeine/controller/IndexControllerTest.java
+++ spring/src/test/java/koffeine/controller/IndexControllerTest.java
@@ -3,6 +3,7 @@ package koffeine.controller;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.http.HttpHeaders;
 import org.springframework.test.web.servlet.MockMvc;
 
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
@@ -28,6 +29,14 @@ public class IndexControllerTest extends AbstractControllerTest {
 			.perform(get("/not-found"))
 			.andExpect(status().isNotFound())
 			.andExpect(content().string(""));
+	}
+
+	@Test
+	public void testCors() throws Exception {
+		mockMvc
+			.perform(get("/").header(HttpHeaders.ORIGIN, "http://not-allowed"))
+			.andExpect(status().isForbidden())
+			.andExpect(content().string("Invalid CORS request"));
 	}
 
 }
