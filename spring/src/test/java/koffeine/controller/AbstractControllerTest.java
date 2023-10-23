7afa7c66b52d3161084c0c05e40eda4c5a343518
Kornél Horváth <kornel.horvath@me.com>
2023-10-24 01:08:37 +0200

    spring-security

--- spring/src/test/java/koffeine/controller/AbstractControllerTest.java
+++ spring/src/test/java/koffeine/controller/AbstractControllerTest.java
@@ -1,10 +1,13 @@
 package koffeine.controller;
 
 import koffeine.AbstractTest;
+import koffeine.configuration.SecurityConfiguration;
 import koffeine.configuration.properties.CorsProperties;
 import org.springframework.context.annotation.Import;
+import org.springframework.security.test.context.support.WithMockUser;
 
-@Import(CorsProperties.class)
+@Import({ CorsProperties.class, SecurityConfiguration.class })
+@WithMockUser(authorities = "user")
 class AbstractControllerTest extends AbstractTest {
 
 }
