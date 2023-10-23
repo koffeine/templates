fbcf3ee57481e6e8fbb3fd1a7836597bad67f6c8
Kornél Horváth <kornel.horvath@me.com>
2023-10-24 01:09:07 +0200

    docker

--- svelte/vite.config.js
+++ svelte/vite.config.js
@@ -1,14 +1,20 @@
 import { svelte } from '@sveltejs/vite-plugin-svelte';
 
 /** @type {import('vite').UserConfigExport} */
-export default {
+export default ({ mode }) => ({
 	plugins: [ svelte() ],
 
 	server: { open: true },
+
+	resolve: {
+		alias: mode === 'production' && [
+			{ find: /^(.*)\/env\.js$/v, replacement: '$1/env.production.js' }
+		]
+	},
 
 	build: {
 		target: 'esnext',
 		rollupOptions: { output: { generatedCode: 'es2015' } },
 		reportCompressedSize: false
 	}
-};
+});
