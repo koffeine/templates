98ee3aee6e590260dc840df3dacd1500d55e6812
Kornél Horváth <kornel.horvath@me.com>
2023-10-24 01:08:37 +0200

    spring-security

--- spring/src/main/java/koffeine/controller/IndexController.java
+++ spring/src/main/java/koffeine/controller/IndexController.java
@@ -1,5 +1,6 @@
 package koffeine.controller;
 
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -7,6 +8,7 @@ import org.springframework.web.bind.annotation.RestController;
 public class IndexController {
 
 	@GetMapping("/")
+	@PreAuthorize("hasAuthority('user')")
 	public void index() {
 	}
 
