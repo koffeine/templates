fc2f311d419236ebacb85f6c00999e6be5d179f3
Kornél Horváth <kornel.horvath@me.com>
2023-10-24 01:07:29 +0200

    spring-data-jpa

---
+++ spring/src/main/java/koffeine/repository/AbstractRepository.java
@@ -0,0 +1,10 @@
+package koffeine.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
+import org.springframework.data.repository.NoRepositoryBean;
+
+@NoRepositoryBean
+public interface AbstractRepository<T> extends JpaRepository<T, Long>, JpaSpecificationExecutor<T> {
+
+}
