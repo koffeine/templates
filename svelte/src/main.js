e99436c84acce16ac0d00ec81f2086820e51b3f9
Kornél Horváth <kornel.horvath@me.com>
2025-02-19 19:33:01 +0100

    Updated to Svelte 5

--- svelte/src/main.js
+++ svelte/src/main.js
@@ -1,9 +1,10 @@
 import './main.css';
 
+import { mount } from 'svelte';
 import * as Router from '@koffeine/svelte-router';
 import App from './App.svelte';
 
 Router.init([
 ]);
 
-export default new App({ target: document.body });
+export default mount(App, { target: document.body });
