a6eb41eec65ef9325036d1b963287cea4bdf6766
Kornél Horváth <kornel.horvath@me.com>
2025-07-13 09:04:28 +0200

    Use @koffeine/eslint-config-svelte

--- svelte/eslint.config.js
+++ svelte/eslint.config.js
@@ -1,33 +1,15 @@
 import koffeine from '@koffeine/eslint-config';
+import koffeineSvelte from '@koffeine/eslint-config-svelte';
 import globals from 'globals';
-import svelteParser from 'svelte-eslint-parser';
-import sveltePlugin from 'eslint-plugin-svelte';
 
 /** @type {import('eslint').Linter.Config[]} */
 export default [
 	...koffeine,
+	...koffeineSvelte,
 	{
 		files: [ 'src/**/*' ],
 		languageOptions: {
 			globals: globals.browser
-		}
-	},
-	{
-		files: [ '**/*.svelte', '**/*.svelte.js' ],
-		languageOptions: {
-			parser: svelteParser
-		}
-	},
-	{
-		files: [ '**/*.svelte' ],
-		processor: sveltePlugin.processors.svelte,
-		plugins: {
-			svelte: sveltePlugin
-		},
-		rules: {
-			// svelte
-			...sveltePlugin.configs['flat/recommended'].reduce((rules, config) => ({ ...rules, ...config.rules }), {}),
-			'svelte/no-inner-declarations': koffeine.reduce((rules, config) => ({ ...rules, ...config.rules }), {})['no-inner-declarations']
 		}
 	}
 ];
