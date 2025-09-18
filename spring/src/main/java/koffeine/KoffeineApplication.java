d6af865c2ee011ea7035dd1a7d31b4c70cc93d82
Kornél Horváth <kornel.horvath@me.com>
2025-09-18 13:54:40 +0200

    Updated Java to 25 and Gradle to 9

--- spring/src/main/java/koffeine/KoffeineApplication.java
+++ spring/src/main/java/koffeine/KoffeineApplication.java
@@ -6,7 +6,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class KoffeineApplication {
 
-	public static void main(String[] args) {
+	static void main(String[] args) {
 		SpringApplication.run(KoffeineApplication.class, args);
 	}
 
