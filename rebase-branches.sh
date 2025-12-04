70a0068149785e24999430fcee3b0e9c930c069a
Kornél Horváth <kornel.horvath@me.com>
2025-12-04 09:06:16 +0100

    Renamed master branch to main

--- rebase-branches.sh
+++ rebase-branches.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 
-for i in $(git branch | grep -v master | awk '{print $1}'); do
+for i in $(git branch | grep -v main | awk '{print $1}'); do
 	git checkout "$i"
-	git rebase master
+	git rebase main
 	git push -f
 done
 
-git checkout master
+git checkout main
